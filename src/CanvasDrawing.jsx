c56e654a3d675138849751b65084e33852e72d45
Felipe Chaves <chavesgfelipe@gmail.com>
2023-04-03 00:42:46 -0400

    initial commit

---
+++ src/CanvasDrawing.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function CanvasDrawing() {
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const ctx = canvas.getContext("2d");
+    ctx.scale(2, 2);
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+
+    ctxRef.current = ctx;
+  }, []);
+
+  function startDrawing({ nativeEvent }) {
+    const { offsetX, offsetY } = nativeEvent;
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  }
+
+  function finishDrawing() {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  }
+
+  function draw({ nativeEvent }) {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    ctxRef.current.lineTo(offsetX, offsetY);
+    ctxRef.current.stroke();
+  }
+
+  function clearCanvas() {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  return (
+    <div style={{ position: "relative" }}>
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
+      <button
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          zIndex: 1,
+        }}
+        onClick={clearCanvas}
+      >
+        Clear
+      </button>
+    </div>
+  );
+}
