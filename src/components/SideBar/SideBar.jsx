87e4c3a1b436e0bd50fe6587687ea91eb8eac439
Felipe Chaves <chavesgfelipe@gmail.com>
2023-04-06 07:23:28 -0400

    added autocomplete on the searchbar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,37 +3,77 @@ import axios from 'axios';
 import './SideBar.scss';
 
 export default function SideBar() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResult, setSearchResult] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [autocompleteResults, setAutocompleteResults] = useState([]);
+    const [searchResult, setSearchResult] = useState(null);
 
-  function handleSearch(event) {
-    setSearchQuery(event.target.value);
-  }
-  
-  function searchMonster() {
-      if (searchQuery.trim() !== '') {
-        axios.get(`http://localhost:8080/monsters?name=${searchQuery}`)
+    function handleSearch(event) {
+        setSearchQuery(event.target.value);
+        }
+
+    function handleAutocomplete(event) {
+        const searchName = event.target.value.trim();
+        if (searchName !== '') {
+        axios
+            .get(`http://localhost:8080/monsters?autocomplete=${searchName}`)
+            .then((response) => {
+            setAutocompleteResults(response.data);
+            })
+            .catch((error) => {
+            console.error(error);
+            });
+        } else {
+        setAutocompleteResults([]);
+        }
+    }
+
+    function handleAutocompleteSelection(event) {
+        const selectedMonsterName = event.target.textContent;
+        setSearchQuery(selectedMonsterName);
+        setAutocompleteResults([]);
+        searchMonster(selectedMonsterName);
+    }
+
+    function searchMonster(name) {
+        axios
+        .get(`http://localhost:8080/monsters?name=${name || searchQuery}`)
         .then((response) => {
-          setSearchResult(response.data);
+            setSearchResult(response.data);
         })
         .catch((error) => {
-          console.error(error);
+            console.error(error);
         });
     }
-  }
-  
 
-  useEffect(() => {
-    console.log(searchResult);
-  }, [searchResult]);
+    function handleSubmit(event) {
+        event.preventDefault();
+        searchMonster();
+    }
 
-  return (
-    <div className='sidebar'>
-      <h1 className='sidebar__title'>DND BattleMap</h1>
-      <div>
-        <input type='text' placeholder='Search' value={searchQuery} onChange={handleSearch} />
-        <button onClick={searchMonster}>Search</button>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        console.log(searchResult);
+    }, [searchResult]);
+
+    return (
+        <div className="sidebar">
+            <h1 className="sidebar__title">DND BattleMap</h1>
+            <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Search"
+                value={searchQuery}
+                onChange={handleSearch}
+                onKeyUp={handleAutocomplete}
+            />
+            <ul className='autocomplete'>
+                {autocompleteResults.map((monster, index) => (
+                <li key={monster.id} data-index={index} onClick={handleAutocompleteSelection}>
+                    {monster.index}
+                </li>
+                ))}
+            </ul>
+            <button className='sidebar__searchBtn' type="submit">Search</button>
+            </form>
+        </div>
+    );
 }
